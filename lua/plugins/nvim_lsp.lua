ebc62fe7e18fb989804d9e99853cf63fd79c8ff7
Jordan Dorey <jordandorey7@gmail.com>
2025-07-19 13:54:03 +1000

    go snippets added

--- lua/plugins/nvim_lsp.lua
+++ lua/plugins/nvim_lsp.lua
@@ -1,24 +1,70 @@
 return {
-	-- Main LSP Configuration
 	'neovim/nvim-lspconfig',
 	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for Neovim
-		{ 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+		{ 'williamboman/mason.nvim', config = true }, -- Must be loaded before dependants
 		'williamboman/mason-lspconfig.nvim',
-		'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-		-- Useful status updates for LSP.
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+		-- Useful status updates for LSP
 		{ 'j-hui/fidget.nvim',       opts = {} },
 
-		-- Allows extra capabilities provided by nvim-cmp
+		-- Autocompletion integration
 		'hrsh7th/cmp-nvim-lsp',
 	},
 	config = function()
-		local server = {
-			gopls = {
-				filetypes = { "go", "gomod", "gowork", "gohtml", "gotmpl", "go.html", "go.tmpl" }
-			}
+		-- This gets the capabilities required by nvim-cmp
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+		-- Get the lspconfig plugin
+		local lspconfig = require('lspconfig')
+
+		-- Configure gopls
+		lspconfig.gopls.setup({
+			-- Pass the capabilities from nvim-cmp
+			capabilities = capabilities,
+
+			-- Custom settings for gopls
+			settings = {
+				gopls = {
+					-- Additional filetypes for gopls to attach to
+					gofumpt = true,
+					staticcheck = true,
+					analyses = {
+						unusedparams = true,
+					},
+				},
+			},
+
+			-- Keymaps and other on_attach logic
+			on_attach = function(client, bufnr)
+				-- Enable completion triggered by typing
+				-- client.server_capabilities.completionProvider = true -- This is usually enabled by default
+
+				local bufmap = function(mode, lhs, rhs)
+					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true })
+				end
+
+				-- Standard LSP keymaps
+				bufmap('n', 'K', vim.lsp.buf.hover)
+				bufmap('n', 'gd', vim.lsp.buf.definition)
+				bufmap('n', 'gD', vim.lsp.buf.declaration)
+				bufmap('n', 'gi', vim.lsp.buf.implementation)
+				bufmap('n', 'gr', vim.lsp.buf.references)
+				bufmap('n', '<leader>ca', vim.lsp.buf.code_action)
+				bufmap('n', '<leader>rn', vim.lsp.buf.rename)
+			end,
+		})
+
+		-- This ensures gopls is installed automatically by mason
+		require('mason-lspconfig').setup_handlers {
+			function(server_name) -- Default handler
+				lspconfig[server_name].setup {
+					capabilities = capabilities,
+				}
+			end,
+			['gopls'] = function()
+				-- The gopls setup is already defined above,
+				-- so we can leave this empty to avoid overriding it.
+			end,
 		}
-	end
+	end,
 }
--- lua/snippets.lua
+++ lua/snippets.lua
@@ -22,6 +22,29 @@ end, { silent = true })
 
 ls.add_snippets("go", {
 	s(
+		"newFile",
+		fmt(
+			[[
+			package {1}
+
+			import (
+				{2}
+			)
+
+			func {3}() {4} {{
+				{5}
+			}}
+			]],
+			{
+				i(1, "name"),
+				i(2, "imports"),
+				i(3, "name"),
+				i(4, "returns"),
+				i(5),
+			}
+		)
+	),
+	s(
 		"nil",
 		fmt(
 			[[
@@ -59,6 +82,105 @@ ls.add_snippets("go", {
 				i(1, "name"),
 				i(2, "var"),
 				i(3, "type"),
+			}
+		)
+	),
+	s(
+		"for",
+		fmt(
+			[[
+			for i:={1}; i {2}; {3} {{
+				{4}
+			}}
+			]],
+			{
+				i(1, "0"),
+				i(2, "< 10"),
+				i(3, "i++"),
+				i(4),
+			}
+		)
+	),
+	s(
+		"for range",
+		fmt(
+			[[
+			for {1}, {2} := range {3} {{
+				{4}
+			}}
+			]],
+			{
+				i(1, "index"),
+				i(2, "value"),
+				i(3, "var"),
+				i(4),
+			}
+		)
+	),
+	s(
+		"array",
+		fmt(
+			[[
+			{1} := [{2}]{3}{{{4}}}
+			]],
+			{
+				i(1, "var"),
+				i(2, "..."),
+				i(3, "type"),
+				i(4),
+			}
+		)
+	),
+	s(
+		"switch",
+		fmt(
+			[[
+			switch {1} {{
+				case {2}:
+					{3}
+				case {4}:
+					{5}
+				default:
+					{6}
+			}}
+			]],
+			{
+				i(1, "var"),
+				i(2, "val"),
+				i(3),
+				i(4, "val"),
+				i(5),
+				i(6),
+			}
+		)
+	),
+	s(
+		"if",
+		fmt(
+			[[
+			if {1}, err := {2}; err != nil {{
+				{3}
+			}}
+			]],
+			{
+				i(1, "var"),
+				i(2, "func"),
+				i(3),
+			}
+		)
+	),
+	s(
+		"lambda",
+		fmt(
+			[[
+			{1} := func() {2} {{
+				{3}
+			}}
+			]],
+			{
+				i(1, "var"),
+				i(2, "returnVal"),
+				i(3),
 			}
 		)
 	),
